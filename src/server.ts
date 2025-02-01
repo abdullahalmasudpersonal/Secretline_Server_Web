0445448b3e01346d65fc1b15305a5c7fb688adb0
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-02-01 22:39:23 +0600

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,20 +18,72 @@ async function secretlineServer() {
         methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
         credentials: true,
       },
-      pingTimeout: 60000,
+      // pingTimeout: 60000,
       transports: ['websocket'],
     });
 
+    const users: { [key: string]: string } = {};
+
     io.on('connection', (socket) => {
+      socket.on('userOnline', (userId) => {
+        users[userId] = socket.id;
+      });
       console.log('A user connected:', socket.id);
 
-      // চ্যাট মেসেজ গ্রহণ এবং ব্রডকাস্ট
       socket.on('sendMessage', (message) => {
-        console.log(message, 'sendMessage');
-        io.emit('receiveMessage', message); // সকল ক্লায়েন্টকে মেসেজ পাঠানো
+        io.emit('receiveMessage', message);
       });
 
+      socket.on('offer', ({ target, offer }) => {
+        const receiverSocketId = users[target];
+        console.log(target, 'target');
+        console.log(receiverSocketId, 'receiverSocketId');
+        if (receiverSocketId) {
+          io.to(receiverSocketId).emit('offer', {
+            sender: socket.id,
+            userId: Object.keys(users).find((key) => users[key] === socket.id),
+            offer,
+          });
+        } else {
+          console.log('User not found or offline.');
+        }
+      });
+
+      // Receiver sends an answer
+      socket.on('answer', ({ target, answer }) => {
+        const targetSocketId = target;
+        console.log(users, 'users');
+        console.log(targetSocketId, 'target');
+        console.log(answer, 'answer');
+        if (targetSocketId) {
+          io.to(targetSocketId).emit('answer', { sender: socket.id, answer });
+        }
+      });
+
+      // ICE Candidate ইভেন্ট
+      socket.on('ice-candidate', ({ target, candidate }) => {
+        const targetSocketId = target;
+        if (targetSocketId) {
+          io.to(targetSocketId).emit('ice-candidate', {
+            sender: socket.id,
+            candidate,
+          });
+        }
+      });
+
+      // // ICE candidates
+      // socket.on('sendIceCandidate', ({ candidate, to }) => {
+      //   socket.to(to).emit('receiveIceCandidate', { candidate });
+      // });
+
       socket.on('disconnect', () => {
+        for (let userId in users) {
+          if (users[userId] === socket.id) {
+            delete users[userId];
+            console.log('User disconnected:', userId);
+            break;
+          }
+        }
         console.log('A user disconnected:', socket.id);
       });
     });
